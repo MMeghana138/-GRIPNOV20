9a669ee9bca8d73714b80e50e934bb6e9ce2c481
Meghana <mmeghana138@gmail.com>
2026-08-22 13:08:00 +0530

    Added string functions

---
+++ Python_Basic/Strings.py
@@ -0,0 +1,29 @@
+###1) To check wether the string is polindrome or now
+# str1='amaama'
+# str2=str1[::-1]
+# if(str1==str2):
+#     print('Polindrom')
+# else:
+#     print('Not a polindrome')
+
+##2) Symmetric or not
+# s1='amaama'
+# half=int(len(s1)/2)
+# print(half)
+# if len(s1)%2==0:
+#     firstHalf=s1[:half]
+#     secondHalf=s1[half:]
+# else:
+#     firstHalf=s1[:half]
+#     secondHalf=s1[half+1:]
+# if firstHalf==secondHalf:
+#     print('symmetric')
+# else:
+#     print('not symmetric')
+
+#3)Reverse word in a string
+# str1='My name is khan's
+# words=str1.split()
+# reversed_words=words[::-1]
+# result=' '.join(reversed_words)
+# print(result)
